0cb3db28030a7d2ac5bae03d510bb7f60049bcbd
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-04-14 11:27:21 -0400

    cleaning up some code

--- script.js
+++ script.js
@@ -12,22 +12,15 @@ function Book (title, author, pages, read) {
 
 var book = new Book("The Meaning of Mariah Carey", "Mariah Carey with Michaela Angela Davis", 349, "read");
 var book2 = new Book("The Guest List", "Lucy Foley", 313, "read");
-console.log(book.info());
+// console.log(book.info());
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-console.log(myLibrary);
-
 addBookToLibrary(book);
-
-console.log(myLibrary);
-
 addBookToLibrary(book2);
-
-console.log(myLibrary);
 
 myLibrary.forEach(book => {
     console.log(book);
-})
+});
